edd21496750dee08d09238751e7f7ff099ce66fb
Narun Raman <narunraman@localhost.localdomain>
2019-12-03 20:10:31 -0800

    migrated all of the storing into python

--- Camflow/disclose_prov.c
+++ Camflow/disclose_prov.c
@@ -6,25 +6,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct Node {
-    int id;
-    union {
-	    activity_t a;
-	    entity_t e;
-    } Data;
-    struct Node* next;
-} Node;
-
-Node* activity_head = NULL;
-
-/* Entity nodes, separated to search easily
- *
- */
-Node* data_head = NULL;
-Node* function_head = NULL;
-Node* library_head = NULL;
-
-//NODES
 //Will continuously get called from Python for each node
 //creating a linked list for the edges to search through and use
 
@@ -32,122 +13,26 @@ void node_agent(char* node) {
 	disclose_agent(node);
 }
 
-void node_environment(char* node){
-	disclose_entity(node);
-}
-
 /* Discloses activity and pushes the activity_t onto a linked list
  * 
  */
-activity_t push_activity(char* node, int id) {
-	Node *a = malloc(sizeof(Node));
-	a -> id = id;
-	a -> Data.a = disclose_activity(node);
-	a -> next = activity_head;
-	activity_head = a;
-	return a -> Data.a;
+activity_t cam_activity(char* node, int id) {
+	return disclose_activity(node);
 }
 
-void push_data_node(char* node, int id) {
-	Node *e = malloc(sizeof(Node));
-	e -> id = id;
-	e -> Data.e = disclose_entity(node);
-	e -> next = data_head;
-	data_head = e;
+entity_t cam_entity(char* node, int id){
+	return disclose_entity(node);
 }
 
-void push_function_node(char *node, int id) {
-	Node *e = malloc(sizeof(Node));
-	e -> id = id;
-	e -> Data.e = disclose_entity(node);
-	e -> next = function_head;
-	function_head = e;
-}
-
-void push_library_node(char *node, int id) {
-	Node *e = malloc(sizeof(Node));
-	e -> id = id;
-	e -> Data.e = disclose_entity(node);
-	e -> next = library_head;
-	library_head = e;
-}
-
-/* These are the functions for the retrieving and disclosing edges
- *
- */
-
-entity_t get_entity(int node_id, Node *e) {
-	while(e != NULL) {
-		if(e -> id == node_id) {
-			return e -> Data.e;
-		}
-		e = e -> next;
-	}
-	printf("The edge's corresponding entity node does not exist!");
-	exit(EXIT_FAILURE);
-}
-
-activity_t get_activity(int node_id, Node *a) {
-	while(a != NULL) {
-		if(a -> id == node_id) {
-			return a -> Data.a;
-		}
-		a = a -> next;
-	}
-	printf("The edge's corresponding activity node does not exist!");
-	exit(EXIT_FAILURE);
-}
-	
-
-activity_t get_act(int node_id) {
-	return get_activity(node_id, activity_head);
+void edge_uses(entity_t from, activity_t to) {
+	disclose_uses(from, to);
 }
 
 
-void edge_member(int from, int to) {
-	//disclose_something(get_entity(from, library_head), get_entity(to, function_head));
+void edge_generates(activity_t from, entity_t to) {
+	disclose_generates(from, to);
 }
 
-void edge_uses_data(int from, int to) {
-	disclose_uses(get_entity(from, data_head), get_activity(to, activity_head));
+void edge_informs(activity_t from, activity_t to) {
+	disclose_informs(from, to);
 }
-
-void edge_uses_function(int from, int to) {
-	disclose_uses(get_entity(from, function_head), get_activity(to, activity_head));
-}
-
-void edge_generates(int from, int to) {
-	disclose_generates(get_activity(from, activity_head), get_entity(to, data_head));
-}
-
-void edge_informs(int from, int to) {
-	disclose_informs(get_activity(from, activity_head), get_activity(to, activity_head));
-}
-
-void printActivities(Node *n) {
-	while(n != NULL) {
-		printf("activity %d is %d\n", n -> id, n -> Data.a);
-		n = n -> next;
-	}
-}
-
-void printEntities(Node *d, Node *f, Node *l) {
-	while(d != NULL) {
-		printf("data %d is %d\n", d -> id, d -> Data.e);
-		d = d -> next;
-	}
-	while(f != NULL) {
-		printf("function %d is %d\n", f -> id, f -> Data.e);
-		f = f -> next;
-	}
-	while(l != NULL) {
-		printf("library %d is %d\n", l -> id, l -> Data.e);
-		l = l -> next;
-	}
-}
-
-void printLists() {
-	printActivities(activity_head);
-	printEntities(data_head, function_head, library_head);
-}
-
--- Camflow/parse_prov.py
+++ Camflow/parse_prov.py
@@ -14,130 +14,93 @@ import sys
 feedcamflow = ctypes.CDLL("./disclose2cam.so")
 
 #JSON File
-data = {}
+provenance_dict = {}
 
-#NODES
+####### HELPER FUNCTIONS ####### 
+
+def extract_id(node):
+    return json.dumps(node).split(":")[-1][:-1]
+
+
+############# NODES ############
 
 def get_agent():
-        # ag_prefix = 'a1'
-        return json.dumps(data['agent'])[1:-1].encode('utf-8')
+    return json.dumps(provenance_dict['agent'])[1:-1].encode('utf-8')
 
 def get_all_activities():
-    activities = data['activity']
-    for activity in activities:
-        ac_prefix = extract_id(activity)
-        activity_i = activities[activity]
-        node = json.dumps(activity) + ":" + json.dumps(activity_i)
-        yield node, ac_prefix
+    activities = provenance_dict['activity']
+    for key, value in activities.items():
+        node = json.dumps(key) + ":" + json.dumps(value)
+        yield node, extract_id(key)
 
-def extract_entity_type(node):
-    string = node.split(":")
-    return ''.join([i for i in string[-1] if not i.isdigit()])
-
-#split this into data (d), library (l), environment, function (f) nodes
 def get_all_entities():
-    entities = data['entity']
-    for entity in entities:
-        # entity_id = extract_id(json.dumps(entity))
-        entity_i = json.dumps(entity) + ":" + json.dumps(entities[entity])
-        if extract_entity_type(entity) == 'd':
-            entity_id = extract_id(entity)
-            get_all_data(entity_i, entity_id)
-        elif extract_entity_type(entity) == 'f': 
-            entity_id = extract_id(entity)
-            get_all_functions(entity_i, entity_id)
-        elif extract_entity_type(entity) == 'l':
-            entity_id = extract_id(entity)
-            get_all_libraries(entity_i, entity_id)
-        else:
-            feedcamflow.node_environment(entity_i.encode('utf-8'))
+    entities = provenance_dict['entity']
+    for key, value in entities.items():
+        node = json.dumps(key) + ":" + json.dumps(value)
+        yield node, extract_id(key)
 
-def get_all_data(data_node, name):
-    feedcamflow.push_data_node(data_node.encode('utf-8'), name)
-    
-def get_all_functions(function_node, name):
-    feedcamflow.push_function_node(function_node.encode('utf-8'), name)
 
-def get_all_libraries(library_node, name):
-    feedcamflow.push_library_node(library_node.encode('utf-8'), name)
-
-#EDGES
-def extract_id(node):
-    node = json.dumps(node)
-    string = node.split(":")
-    # return int(''.join([i for i in string[-1] if i.isdigit()]))
-    try:
-        x = int(''.join(filter(str.isdigit, string[-1])))
-        return x
-    except ValueError:
-        print("oops! that node did not have a numeral designator.")
+############ EDGES ############
 
 def get_all_p2p():
-    if 'wasInformedBy' in data:
-        return get_all_edges(data['wasInformedBy'], 'prov:informant', 'prov:informed')
+    if 'wasInformedBy' in provenance_dict:
+        return get_all_edges(provenance_dict['wasInformedBy'], 'prov:informant', 'prov:informed')
 
 def get_all_p2d():
-    if 'wasGeneratedBy' in data:
-        return get_all_edges(data['wasGeneratedBy'], 'prov:activity', 'prov:entity')
-
-def get_all_d2p(from_data, to_procedure):
-    feedcamflow.edge_uses_data(from_data, to_procedure)
-
-def get_all_f2p(from_function, to_procedure):
-    feedcamflow.edge_uses_function(from_function, to_procedure)
+    if 'wasGeneratedBy' in provenance_dict:
+        return get_all_edges(provenance_dict['wasGeneratedBy'], 'prov:activity', 'prov:entity')
 
 def get_all_df2p():
-    df2p_edges = data['used']
-    for df2p in df2p_edges:
-        if extract_entity_type(df2p_edges[df2p]['prov:entity']) == 'd':
-            get_all_d2p(extract_id(json.dumps(df2p_edges[df2p]['prov:entity'])), extract_id(json.dumps(df2p_edges[df2p]['prov:activity'])))
-        elif extract_entity_type(df2p_edges[df2p]['prov:activity']) == 'f':
-            get_all_f2p(extract_id(json.dumps(df2p_edges[df2p]['prov:entity'])), extract_id(json.dumps(df2p_edges[df2p]['prov:activity'])))
+    if 'used' in provenance_dict:
+        return get_all_edges(provenance_dict['used'], 'prov:entity', 'prov:activity')
 
 def get_all_l2f():
-    if 'hadMember' in data:
-        return get_all_edges(data['hadMember'], 'prov:collection', 'prov:entity')
+    if 'hadMember' in provenance_dict:
+        return get_all_edges(provenance_dict['hadMember'], 'prov:collection', 'prov:entity')
     
 def get_all_edges(edge_type, from_node, to_node):
     edges = edge_type
     for edge in edges:
         edge_prefix = edge
-        yield(extract_id(json.dumps(edges[edge][from_node])), extract_id(json.dumps(edges[edge][to_node])))
+        yield(extract_id(edges[edge][from_node]), extract_id(edges[edge][to_node]))
 
 
 ################################################ USER LEVEL API ################################################
 
 disclosed_activities = {}
+disclosed_entities = {}
 
 def import_prov(filename):
-    global data
+    global provenance_dict
     with open(filename) as f:
-        data = json.load(f)
+        provenance_dict = json.load(f)
 
 def disclose_agent():
     feedcamflow.disclose_agent(get_agent())
 
 def disclose_activities():
     for activity, act_id in get_all_activities():
-        disclosed_activities[act_id] = feedcamflow.push_activity(activity.encode('utf-8'), act_id)
+        disclosed_activities[act_id] = feedcamflow.cam_activity(activity.encode('utf-8'))
     
 def disclose_entities():
-    get_all_entities()
+    for entity, en_id in get_all_entities():
+        disclosed_entities[en_id] = feedcamflow.cam_entity(entity.encode('utf-8'))
 
 def disclose_used():
-    get_all_df2p()
+    for from_entity, to_activity in get_all_df2p():
+        feedcamflow.edge_uses(disclosed_entities[from_entity], disclosed_activities[to_activity])
 
 def disclose_generated():
     for from_activity, to_entity in get_all_p2d():
-        feedcamflow.edge_generates(from_activity, to_entity)
+        feedcamflow.edge_generates(disclosed_activities[from_activity], disclosed_entities[to_entity])
 
 def disclose_informed():
     for from_activity, to_activity in get_all_p2p():
-        feedcamflow.edge_informs(from_activity, to_activity)
+        feedcamflow.edge_informs(disclosed_activities[from_activity], disclosed_activities[to_activity])
 
 def disclose_member():
     for from_library, to_function in get_all_l2f():
-        feedcamflow.edge_member(from_library, to_function)
+        feedcamflow.edge_member(disclosed_entities[from_library], disclosed_entities[to_function])
 
 def main():
     if len(sys.argv) < 2:
@@ -145,10 +108,9 @@ def main():
     if len(sys.argv) > 2:
         sys.exit("Too many arguments, must be of the form: python parse_prov.py <some json file>")
     import_prov(sys.argv[1])
-    disclose_agent()
+    # disclose_agent()
     disclose_activities()
     disclose_entities()
-    # feedcamflow.printList()
     disclose_used()
     disclose_generated()
     disclose_informed()
