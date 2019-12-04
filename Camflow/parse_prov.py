6034d47875f3514cb85202f7042469698071375b
Narun Raman <narunraman@localhost.localdomain>
2019-12-03 22:49:49 -0800

    reformatted to work better with camflow and camtool

--- Camflow/parse_prov.py
+++ Camflow/parse_prov.py
@@ -11,10 +11,11 @@ import ctypes
 import string
 import sys
 
-feedcamflow = ctypes.CDLL("./disclose2cam.so")
 
-#JSON File
+
+feedcamflow = ctypes.CDLL("./disclose2cam.so")
 provenance_dict = {}
+fine_grained = False
 
 ####### HELPER FUNCTIONS ####### 
 
@@ -25,19 +26,29 @@ def extract_id(node):
 ############# NODES ############
 
 def get_agent():
-    return json.dumps(provenance_dict['agent'])[1:-1].encode('utf-8')
+    return json.dumps(provenance_dict['agent'])[1:-1]
 
 def get_all_activities():
     activities = provenance_dict['activity']
     for key, value in activities.items():
-        node = json.dumps(key) + ":" + json.dumps(value)
-        yield node, extract_id(key)
+        activity = ""
+        act_id = extract_id(key)
+        if(not fine_grained):
+            activity = json.dumps([val for key, val in value.items() if 'name' in key.lower()][0])
+        else:
+            activity = json.dumps(key) + ":" + json.dumps(value)
+        yield ("\"prov:label\":" + activity).encode('utf-8'), act_id
 
 def get_all_entities():
     entities = provenance_dict['entity']
     for key, value in entities.items():
-        node = json.dumps(key) + ":" + json.dumps(value)
-        yield node, extract_id(key)
+        entity = ""
+        en_id = extract_id(key)
+        if(not fine_grained):
+            entity = json.dumps([val for key, val in value.items() if 'name' in key.lower()][0])
+        else:
+            entity = json.dumps(key) + ":" + json.dumps(value)
+        yield ("\"prov:label\":" + entity).encode('utf-8'), en_id
 
 
 ############ EDGES ############
@@ -75,16 +86,28 @@ def import_prov(filename):
     with open(filename) as f:
         provenance_dict = json.load(f)
 
+def set_granularity(granularity):
+    if(type(granularity) == bool):
+        parse_prov.fine_grained = granularity
+    else:
+        print("Please provide a boolean value for granularity")
+
+def get_granularity():
+    if(parse_prov.fine_grained):
+        print("fine granularity")
+    else:
+        print("regular granularity")
+
 def disclose_agent():
-    feedcamflow.disclose_agent(get_agent())
+    feedcamflow.disclose_agent(get_agent().encode('utf-8'))
 
 def disclose_activities():
     for activity, act_id in get_all_activities():
-        disclosed_activities[act_id] = feedcamflow.cam_activity(activity.encode('utf-8'))
+        disclosed_activities[act_id] = feedcamflow.cam_activity(activity)
     
 def disclose_entities():
     for entity, en_id in get_all_entities():
-        disclosed_entities[en_id] = feedcamflow.cam_entity(entity.encode('utf-8'))
+        disclosed_entities[en_id] = feedcamflow.cam_entity(entity)
 
 def disclose_used():
     for from_entity, to_activity in get_all_df2p():
@@ -102,6 +125,13 @@ def disclose_member():
     for from_library, to_function in get_all_l2f():
         feedcamflow.edge_member(disclosed_entities[from_library], disclosed_entities[to_function])
 
+def disclose_prov():
+    disclose_activities()
+    disclose_entities()
+    disclose_used()
+    disclose_generated()
+    disclose_informed()
+
 def main():
     if len(sys.argv) < 2:
         sys.exit("Missing argument, must be of the form: python parse_prov.py <some json file>")
@@ -109,12 +139,7 @@ def main():
         sys.exit("Too many arguments, must be of the form: python parse_prov.py <some json file>")
     import_prov(sys.argv[1])
     # disclose_agent()
-    disclose_activities()
-    disclose_entities()
-    disclose_used()
-    disclose_generated()
-    disclose_informed()
-
+    disclose_prov()
 
 if __name__ == "__main__":
     main()
