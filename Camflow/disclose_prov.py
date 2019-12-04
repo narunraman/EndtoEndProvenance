a4b2e5cef0a218bf07209767d1f44ad17e13a47b
Narun Raman <narunraman@localhost.localdomain>
2019-12-04 00:00:48 -0800

    added json and txt examples

--- Camflow/disclose_prov.py
+++ Camflow/disclose_prov.py
@@ -25,7 +25,7 @@ def get_granularity():
         print("regular granularity")
 
 def disclose_agent():
-    feedcamflow.disclose_agent(get_agent().encode('utf-8'))
+    feedcamflow.disclose_agent(parse_prov.get_agent())
 
 def disclose_activities():
     for activity, act_id in parse_prov.get_all_activities():
@@ -36,27 +36,33 @@ def disclose_entities():
         parse_prov.disclosed_entities[en_id] = feedcamflow.cam_entity(entity)
 
 def disclose_used():
-    for from_entity, to_activity in parse_prov.get_all_df2p():
-        feedcamflow.edge_uses(parse_prov.disclosed_entities[from_entity], parse_prov.disclosed_activities[to_activity])
+    if parse_prov.get_all_df2p() != None:
+        for from_entity, to_activity in parse_prov.get_all_df2p():
+            feedcamflow.edge_uses(parse_prov.disclosed_entities[from_entity], parse_prov.disclosed_activities[to_activity])
 
 def disclose_generated():
-    for from_activity, to_entity in parse_prov.get_all_p2d():
-        feedcamflow.edge_generates(parse_prov.disclosed_activities[from_activity], parse_prov.disclosed_entities[to_entity])
+    if parse_prov.get_all_p2d() != None:
+        for from_activity, to_entity in parse_prov.get_all_p2d():
+            feedcamflow.edge_generates(parse_prov.disclosed_activities[from_activity], parse_prov.disclosed_entities[to_entity])
 
 def disclose_informed():
-    for from_activity, to_activity in parse_prov.get_all_p2p():
-        feedcamflow.edge_informs(parse_prov.disclosed_activities[from_activity], parse_prov.disclosed_activities[to_activity])
+    if parse_prov.get_all_p2p() != None:
+        for from_activity, to_activity in parse_prov.get_all_p2p():
+            feedcamflow.edge_informs(parse_prov.disclosed_activities[from_activity], parse_prov.disclosed_activities[to_activity])
 
 def disclose_member():
-    for from_library, to_function in parse_prov.get_all_l2f():
-        feedcamflow.edge_member(parse_prov.disclosed_entities[from_library], parse_prov.disclosed_entities[to_function])
+    if parse_prov.get_all_l2f() != None:
+        for from_library, to_function in parse_prov.get_all_l2f():
+            feedcamflow.edge_member(parse_prov.disclosed_entities[from_library], parse_prov.disclosed_entities[to_function])
 
 def disclose_prov():
+    disclose_agent()
     disclose_activities()
     disclose_entities()
     disclose_used()
     disclose_generated()
     disclose_informed()
+    disclose_member()
 
 def main():
     if len(sys.argv) < 2:
@@ -64,7 +70,6 @@ def main():
     if len(sys.argv) > 2:
         sys.exit("Too many arguments, must be of the form: python parse_prov.py <some json file>")
     import_prov(sys.argv[1])
-    # disclose_agent()
     disclose_prov()
 
 if __name__ == "__main__":
